53a73c7d5ce600110163db2136ed9ee8120530c0
marcel <marcel@chuj.pl>
2020-08-29 17:56:15 +0200

    switching from htmlunit to jsoup library

--- src/main/java/org/marcel/services/OnetWeatherService.java
+++ src/main/java/org/marcel/services/OnetWeatherService.java
@@ -1,25 +1,28 @@
 package org.marcel.services;
 
+import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+import com.gargoylesoftware.htmlunit.html.*;
 import org.marcel.weatherclasses.WeatherResponse;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class OnetWeatherService implements WeatherService {
 
     public WeatherResponse getWeather(String place, LocalDate date) throws IOException {
-        WebClient webClient = new WebClient();
-        HtmlPage startingPage = webClient.getPage("https://www.google.pl/");
-        HtmlTextInput googleSearch = startingPage.getElementByName("q");
-        googleSearch.setText(prepareQuerySring(place));
-        System.out.println(googleSearch.getText());
-        System.out.println(place);
-        System.out.println(date);
+//        WebClient webClient = new WebClient();
+//        webClient.getOptions().setThrowExceptionOnScriptError(false);
+//        webClient.getOptions().setJavaScriptEnabled(false);
+//        HtmlPage startingPage = webClient.getPage("https://www.google.pl/");
+//        HtmlTextInput searchBar = startingPage.getElementByName("q");
+//        searchBar.setText(prepareQuerySring(place));
+//        HtmlSubmitInput submitSearch = startingPage.getElementByName("btnK");
+//        HtmlPage resultPage = submitSearch.click();
+//        List<HtmlDivision> resultDivs = resultPage.getByXPath("div[@class='g']");
         return new WeatherResponse();
     }
 
